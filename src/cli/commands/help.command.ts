3156ed958211fff3c9c12dea2c17bf395307fa45
curious_andrew <afirsov410@gmail.com>
2023-10-12 11:33:23 +0300

    заюзал чалк

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,20 +1,22 @@
 import {Command} from './types.js';
-// import {Chalk} from 'chalk'
+import {Chalk} from 'chalk'
+
+const chalk = new Chalk()
 
 export class HelpCommand implements Command {
-  public getName(): string {
-    return '--help';
-  }
+    public getName(): string {
+        return '--help';
+    }
 
-  public async execute(..._parameters: string[]): Promise<void> {
-    console.info(`
-        Программа для подготовки данных для REST API сервера.
-        Пример:
+    public async execute(..._parameters: string[]): Promise<void> {
+        console.info(`
+        ${chalk.bold('Программа для подготовки данных для REST API сервера.')}
+        ${chalk.red('Пример:')}
             cli.js --<command> [--arguments]
-        Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
+        ${chalk.red('Команды:')}
+            --version:                   ${chalk.cyan('# выводит номер версии')}
+            --help:                      ${chalk.cyan('# печатает этот текст')}
+            --import <path>:             ${chalk.cyan('# импортирует данные из TSV')}
     `);
-  }
+    }
 }
